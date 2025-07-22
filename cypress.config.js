196340c11f3e8ced9de336e67a8d2bac15f65fa1
Walter Diaz <walter.diaz@qualifiedigital.com>
2025-07-22 16:18:55 -0600

    Create a new configurate in cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: 'ndiquy', 
+  pageLoadTimeout: 300000, 
   e2e: {
     setupNodeEvents(on, config) {
       
