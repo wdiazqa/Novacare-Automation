0982e7c846b94edf20ecad9d749cf0a4f84cd77c
Walter Diaz <walter.diaz@qualifiedigital.com>
2025-07-23 01:07:15 -0600

    change configurations

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: 'ndiquy', 
-  pageLoadTimeout: 300000, 
   e2e: {
     setupNodeEvents(on, config) {
       
     },
   },
-})
+}) 
