4d5def26f1d90498335601645c3052152490cdc1
Walter Diaz <walter.diaz@qualifiedigital.com>
2025-07-27 19:44:40 -0600

    create browser

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   projectId: 'ndiquy', 
   pageLoadTimeout: 600000,
+  defaultCommandTimeout: 60000,
   e2e: {
     setupNodeEvents(on, config) {
       
